ee3c02357b2ae9ab4694684b8f29682d27feae83
schulle0815 <andreas.schulze79@gmx.de>
2018-11-27 13:35:04 +0100

    first

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,9 @@
-import { interval, Observable, of } from "rxjs";
-import { flatMap, map, switchMap, take, tap } from "rxjs/operators";
+import {interval, Observable, of} from 'rxjs';
+import {flatMap, map, switchMap, take, tap} from 'rxjs/operators';
 
-console.log("test");
+console.log('test');
 
 let o = interval(2000).pipe(take(5));
 //.pipe(tap(d => console.log("Interval!")));
-o.subscribe(d => console.log("Event 1!"));
-o.subscribe(d => console.log("Event 2!"));
+o.subscribe(d => console.log('Event 1!'));
+o.subscribe(d => console.log('Event 2!'));
