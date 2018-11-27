fe52ad5632ca58c27e4a1594bc101abca82d8b2a
schulle0815 <andreas.schulze79@gmx.de>
2018-11-27 13:13:55 +0100

    First commit

---
+++ src/data.ts
@@ -0,0 +1,40 @@
+import { interval, Observable, of } from "rxjs";
+import { toPromise } from "rxjs/operator/toPromise";
+import {
+  filter,
+  flatMap,
+  map,
+  merge,
+  switchMap,
+  take,
+  tap,
+  zip
+} from "rxjs/operators";
+
+console.log("test");
+
+let data = ["One", "Two", "Three", "Four", "Five"];
+let data2 = ["One", "Two", "Three", "Four", "Five"];
+let data3 = ["A", "B", "C", "D", "E"];
+
+// rx chain
+let o = of(...data);
+o.pipe(map(d => d + ":"))
+  .pipe(filter(d => d !== "Two:"))
+  .pipe(tap(d => console.log(d)))
+  .subscribe();
+
+// rx to promise conversion
+let p = of(...data);
+p.pipe(map(d => d + ":"))
+  .pipe(filter(d => d !== "Two:"))
+  .pipe(tap(d => console.log(d)))
+  .toPromise()
+  .then(() => console.log("Promise success!"));
+
+let o1 = of(...data2);
+let o2 = of(...data3);
+
+o1.pipe(zip(o2)).subscribe(d => console.log(d));
+
+o1.pipe(merge(o2)).subscribe(d => console.log(d));
---
+++ src/test.ts
@@ -0,0 +1,9 @@
+import { interval, Observable, of } from "rxjs";
+import { flatMap, map, switchMap, take, tap } from "rxjs/operators";
+
+console.log("test");
+
+let o = interval(2000).pipe(take(5));
+//.pipe(tap(d => console.log("Interval!")));
+o.subscribe(d => console.log("Event 1!"));
+o.subscribe(d => console.log("Event 2!"));
