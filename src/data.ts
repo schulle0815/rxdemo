3ed5ed03551238bd789d9720432710fcdb78d609
schulle0815 <andreas.schulze79@gmx.de>
2018-11-27 13:21:02 +0100

    first

--- src/data.ts
+++ src/data.ts
@@ -1,36 +1,24 @@
-import { interval, Observable, of } from "rxjs";
-import { toPromise } from "rxjs/operator/toPromise";
-import {
-  filter,
-  flatMap,
-  map,
-  merge,
-  switchMap,
-  take,
-  tap,
-  zip
-} from "rxjs/operators";
+import {interval, Observable, of} from 'rxjs';
+import {toPromise} from 'rxjs/operator/toPromise';
+import {filter, flatMap, map, merge, switchMap, take, tap, zip} from 'rxjs/operators';
 
-console.log("test");
+console.log('test');
 
-let data = ["One", "Two", "Three", "Four", "Five"];
-let data2 = ["One", "Two", "Three", "Four", "Five"];
-let data3 = ["A", "B", "C", "D", "E"];
+let data = ['One', 'Two', 'Three', 'Four', 'Five'];
+let data2 = ['One', 'Two', 'Three', 'Four', 'Five'];
+let data3 = ['A', 'B', 'C', 'D', 'E'];
 
 // rx chain
 let o = of(...data);
-o.pipe(map(d => d + ":"))
-  .pipe(filter(d => d !== "Two:"))
-  .pipe(tap(d => console.log(d)))
-  .subscribe();
+o.pipe(map(d => d + ':')).pipe(filter(d => d !== 'Two:')).pipe(tap(d => console.log(d))).subscribe();
 
 // rx to promise conversion
 let p = of(...data);
-p.pipe(map(d => d + ":"))
-  .pipe(filter(d => d !== "Two:"))
-  .pipe(tap(d => console.log(d)))
-  .toPromise()
-  .then(() => console.log("Promise success!"));
+p.pipe(map(d => d + ':'))
+    .pipe(filter(d => d !== 'Two:'))
+    .pipe(tap(d => console.log(d)))
+    .toPromise()
+    .then(() => console.log('Promise success!'));
 
 let o1 = of(...data2);
 let o2 = of(...data3);
